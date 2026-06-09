d9ed2786ba0c7dbb7b62b049e36097ba28ec1b0f
stevejwitt-png <stevejwitt@gmail.com>
2026-06-09 09:18:16 +0100

    Change Cache-Control header to no-cache, no-store

--- api/poll.js
+++ api/poll.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
   }
 
   res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
   res.send(`<!DOCTYPE html><html><head>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="${title}"/>
